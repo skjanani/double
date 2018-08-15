4782060de68ac6e2a20b85cd3aefadf2e032ab05
skjanani <41111202+skjanani@users.noreply.github.com>
2018-08-15 11:20:53 +0530

    Update ModuloDivison.java

--- ModuloDivison.java
+++ ModuloDivison.java
@@ -4,17 +4,12 @@ public class ModuloDivison {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//int n,n1,a,b;
-double n,n1;
-double a=0;
+int n,n1;
+int a=0;
 System.out.println("enter the number");
 Scanner ja=new Scanner(System.in);
-n=ja.nextDouble();
-n1=ja.nextDouble();
-/*a=n%n1;
-b=n/n1;
-System.out.println(a);
-System.out.println(b);*/
+n=ja.nextInt();
+n1=ja.nextInt();
 a=n1-n;
 System.out.println(a);
 	}
